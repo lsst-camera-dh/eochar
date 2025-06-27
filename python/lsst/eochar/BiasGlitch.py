Fix bias noise and average

python/lsst/eochar/BiasGlitch.py
@@ -54,7 +54,7 @@ def ProcessNoise(run_cur,raft_cur,ccd_cur,exp_id,plot=True,show=False):
             for iamp in range(nb_amp) :
                 plt.subplot(4,int(nb_amp/4),iamp+1)
                 if imet==0 :
-                    label='%s,%s=%4.2f,%s=%4.2f' % (ch[iamp],noise_met[1],np.median(ampnoise[:,iamp,1,0]),noise_met[2],np.median(ampnoise[:,iamp,2,0]))
+                    label='%s,%s=%4.2f' % (ch[iamp],noise_met[0],np.median(ampnoise[:,iamp,0,0])))
                     plt.gca().set_title(label)
                 plt.plot(range(nb_exp),ampnoise[:,iamp,imet,0],color=color[imet])
                 if iamp>=nb_amp-4 :
@@ -76,7 +76,7 @@ def ProcessNoise(run_cur,raft_cur,ccd_cur,exp_id,plot=True,show=False):
         for imet in range(1,3) : 
             for iamp in range(nb_amp) :
                 plt.subplot(4,int(nb_amp/4),iamp+1)
-                if imet==0 :
+                if imet==1 :
                     label='%s,%s=%4.2f,%s=%4.2f' % (ch[iamp],noise_met[1],np.median(ampnoise[:,iamp,1,0]),noise_met[2],np.median(ampnoise[:,iamp,2,0]))
                     plt.gca().set_title(label)
                 plt.plot(range(nb_exp),ampnoise[:,iamp,imet,0],color=color[imet])
@@ -100,7 +100,7 @@ def ProcessNoise(run_cur,raft_cur,ccd_cur,exp_id,plot=True,show=False):
             for iamp in range(nb_amp) :
                 plt.subplot(4,int(nb_amp/4),iamp+1)
                 if imet==0 :
-                    label='%s,%s=%4.2f,%s=%4.2f' % (ch[iamp],noise_met[1],np.median(ampnoise[:,iamp,1,1]),noise_met[2],np.median(ampnoise[:,iamp,2,1]))
+                    label='%s,%s=%4.2f' % (ch[iamp],noise_met[0],np.median(ampnoise[:,iamp,0,1]))
                     plt.gca().set_title(label)
                 plt.plot(range(nb_exp),ampnoise[:,iamp,imet,1],color=color[imet])
                 if iamp>=nb_amp-4 :
@@ -123,7 +123,7 @@ def ProcessNoise(run_cur,raft_cur,ccd_cur,exp_id,plot=True,show=False):
         for imet in range(1,3) : 
             for iamp in range(nb_amp) :
                 plt.subplot(4,int(nb_amp/4),iamp+1)
-                if imet==0 :
+                if imet==1 :
                     label='%s,%s=%4.2f,%s=%4.2f' % (ch[iamp],noise_met[1],np.median(ampnoise[:,iamp,1,1]),noise_met[2],np.median(ampnoise[:,iamp,2,1]))
                     plt.gca().set_title(label)
                 plt.plot(range(nb_exp),ampnoise[:,iamp,imet,1],color=color[imet])

python/lsst/eochar/ImageAna.py
@@ -70,7 +70,7 @@ class ImageAna :
         self.image[index,:,:]=image_2d-(cor_l+cor_c)
         # Noise analysis
         if noise_analysis :
-                # noise in most of the image area per 5x5 bin after bias subtraction
+                # noise in most of the imageself. area per 5x5 bin after bias subtraction
                 ny,nx=np.shape(self.image[index,self.first_l:self.first_l_over,self.first_c:self.first_c_over])
                 nx5=int(nx/5)
                 ny5=int(ny/5)
@@ -83,10 +83,10 @@ class ImageAna :
                 self.amp_noise[index,0,0]=np.median(E)
                 self.amp_noise[index,0,1]=np.median(np.sqrt(EX2-E**2))
                 # noise in  the serial overscan
-                self.amp_noise[index,1,0]=np.median(image_2d[self.first_l+skip_l_over:,self.first_c_over+skip_c_over:])
+                self.amp_noise[index,1,0]=np.median(image_2d[self.first_l+skip_l_over:,self.first_c_over+skip_c_over:].mean(axis=1))
                 self.amp_noise[index,1,1]=np.median(image_2d[self.first_l+skip_l_over:,self.first_c_over+skip_c_over:].std(axis=1))
                 # noise in  the // overscan 
-                self.amp_noise[index,2,0]=np.median(image_2d[self.first_l_over+skip_l_over:,self.first_c+skip_c_over:])       
+                self.amp_noise[index,2,0]=np.median(image_2d[self.first_l_over+skip_l_over:,self.first_c+skip_c_over:].mean(axis=0))       
                 self.amp_noise[index,2,1]=np.median(image_2d[self.first_l_over+skip_l_over:,self.first_c+skip_c_over:].std(axis=0))       
         # create a view of the science part of the CCD image
         # self.science_image=self.image_raw[self.first_l:self.first_l_over,slef.first_c:self.first_c_over]